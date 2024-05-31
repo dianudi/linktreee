c6c30e491f03ab90c1f9dcac384d9a88a1b92aa6
dianudi <144350056+dianudi@users.noreply.github.com>
2024-05-31 22:47:43 +0700

    add media share modal

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
 import "./style.css";
+import Alpine from "alpinejs";
+import type { Alpine as alpineType } from "alpinejs";
+
+declare global {
+  interface Window {
+    Alpine: alpineType;
+  }
+}
+window.Alpine = Alpine;
+
+Alpine.start();
